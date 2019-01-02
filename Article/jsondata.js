7440918f169bfcc11510b696c254d2588b94599c
InfinaCanada <43710241+InfinaCanada@users.noreply.github.com>
2019-01-01 20:20:25 -0400

    New article viewer

---
+++ Article/jsondata.js
@@ -0,0 +1,53 @@
+Title([
+{
+"Title": "Template",
+},
+])
+Author([
+{
+"Author": "Firstname Lastname",
+},
+])
+LastEdit([
+{
+"LastEdit": "2019 01 01",
+},
+])
+PublishedOn([
+{
+"PublishedOn": "2018 06 28",
+},
+])
+City([
+{
+"City": "Saint John N.B. Canada"
+},
+])
+Website([
+{
+"Website": "Infina Canada"
+},
+])
+Publisher([
+{
+"Publisher": "Infina Canada"
+},
+])
+Body([
+{
+"SubTitle": "JavaScript Tutorial",
+"Text": "https://www.w3schools.com/js/default.asp"
+},
+{
+"SubTitle": "HTML Tutorial",
+"Text": "https://www.w3schools.com/html/default.asp"
+},
+{
+"SubTitle": "CSS Tutorial",
+"Text": "https://www.w3schools.com/css/default.asp"
+},
+{
+"SubTitle": "Test",
+"Text": "some text"
+}
+]);
